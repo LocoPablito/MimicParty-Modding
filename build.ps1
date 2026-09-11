73b78191ca599c488691f713872488ef95c01304
arribbaa <squachi.m@gmail.com>
2026-09-11 15:30:54 +0200

    Make build script Core-only

--- build.ps1
+++ build.ps1
@@ -6,14 +6,12 @@ if (-not (Get-Command dotnet -ErrorAction SilentlyContinue)) {
     throw ".NET SDK not found. Install .NET 8 SDK (or newer), then run this script again."
 }
 
+$Project = Join-Path $Root "src\MimicParty.ModdingCore\MimicParty.ModdingCore.csproj"
+
 Push-Location $Root
 try {
-    dotnet restore .\src\MimicParty.ModdingCore\MimicParty.ModdingCore.csproj
-    dotnet restore .\src\MimicParty.TenPlayerExpansion\MimicParty.TenPlayerExpansion.csproj
-
-    dotnet build .\src\MimicParty.ModdingCore\MimicParty.ModdingCore.csproj -c Release --no-restore
-    dotnet build .\src\MimicParty.TenPlayerExpansion\MimicParty.TenPlayerExpansion.csproj -c Release --no-restore
-
+    dotnet restore $Project
+    dotnet build $Project -c Release --no-restore
     & "$Root\package.ps1"
 }
 finally {
