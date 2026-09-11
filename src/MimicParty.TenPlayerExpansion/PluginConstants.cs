cca16ff8c16b5aeb23a7f752adbc65aa6fe1b08d
arribbaa <squachi.m@gmail.com>
2026-09-11 15:33:21 +0200

    Remove Expansion source from Core repository

--- src/MimicParty.TenPlayerExpansion/PluginConstants.cs
+++
@@ -1,9 +0,0 @@
-namespace Arribbaa.MimicParty.TenPlayerExpansion;
-
-public static class PluginConstants
-{
-    public const string Guid = "com.arribbaa.mimicparty.10playerexpansion";
-    public const string Name = "Mimic Party - 10 Player Expansion";
-    public const string Version = "1.1.0";
-    public const string ProcessName = "Mimic Party.exe";
-}
